4861537f1347bb7740cda672813a9abda39470c6
Igdálias Cabamba <igdaliasmussulveiracabamba@gmail.com>
2022-07-31 00:02:53 +0100

    refactored

---
+++ src/console.rs
@@ -0,0 +1,83 @@
+use termimad::{*, minimad::TextTemplate};
+use chrono::{Timelike, Utc};
+use crossterm::style::Color;
+
+pub(crate) struct Console {
+    console: MadSkin,
+}
+impl Console {
+    pub fn new() -> Console {
+        Console {
+            console: MadSkin::default(),
+        }
+    }
+
+    pub fn print_start(&self) {
+        let now = Utc::now();
+        let hour = now.hour();
+
+        println!(
+            "Starting at {:02}:{:02}:{:02}",
+            hour,
+            now.minute(),
+            now.second(),
+        );
+    }
+
+    pub fn print_output(&mut self, stdout: String) {
+        let output_template = TextTemplate::from(r#"* OUTPUT: **${text}**"#);
+        let mut output_expander = output_template.expander();
+
+        output_expander.set("text", stdout.as_str());
+        self.console.print_expander(output_expander);
+    }
+
+    pub fn print_exit_status_sucess(&mut self, bin: String, status_code: String, bin_args: String) {
+        self.console.bold.set_fg(crossterm::style::Color::Yellow);
+        self.console.italic.set_fg(crossterm::style::Color::Blue);
+        self.console
+            .paragraph
+            .set_fg(crossterm::style::Color::Green);
+
+        let text_template = TextTemplate::from(
+            r#"
+        * ran: **${program}**
+        * code: **${status-code}**
+        * *args*: **${program-args} **
+        "#,
+        );
+        let mut expander = text_template.expander();
+        expander
+            .set("program", &bin)
+            .set("status-code", &status_code)
+            .set("program-args", &bin_args);
+        self.console.print_expander(expander);
+    }
+    pub fn print_exit_status_fail(&mut self, bin: String) {
+        self.console.bold.set_fg(crossterm::style::Color::Red);
+        self.console.italic.set_fg(crossterm::style::Color::Grey);
+
+        let text_template = TextTemplate::from(r#"**Failed to execute**: *${program}*"#);
+        let mut expander = text_template.expander();
+
+        expander.set("program", &bin);
+        self.console.print_expander(expander);
+    }
+
+    pub fn print_end(&self) {
+        let now = Utc::now();
+        let hour = now.hour();
+
+        println!(
+            "Finishing at {:02}:{:02}:{:02}",
+            hour,
+            now.minute(),
+            now.second(),
+        );
+    }
+    pub fn print_exeption(&mut self, bin: String) -> String {
+        let base_excpetion_msg: String = "Failed to load ".to_owned();
+        let excpetion_log = base_excpetion_msg + &bin;
+        return excpetion_log;
+    }
+}
--- src/core.rs
+++ src/core.rs
@@ -1,81 +1,34 @@
+use crate::{console};
 use std::process::Command;
-use termimad::*;
-use crossterm::style::Color;
-use chrono::{Timelike, Utc};
-use crate::core::minimad::TextTemplate;
 
-pub fn new_process(bin:&str, args:&Vec<std::string::String>) -> std::process::ExitStatus{
-    let now = Utc::now();
+pub fn new_process(bin: &str, args: &Vec<std::string::String>) -> std::process::ExitStatus {
+    let mut console_api = console::Console::new();
 
-    let hour = now.hour();
-    println!(
-        "Starting at {:02}:{:02}:{:02}",
-        hour,
-        now.minute(),
-        now.second(),
-    );
-
-    let base_excpetion_msg:String = "Failed to load ".to_owned();
-    let excpetion_log = base_excpetion_msg + bin;
+    console_api.print_start();
 
     let process = Command::new(bin)
         .args(args)
-        //.status()
         .output()
-        .expect(&excpetion_log);
-    
+        .expect(&console_api.print_exeption(bin.to_owned()));
+
     let out = std::string::String::from_utf8(process.stdout);
-    println!("{:#?}", out);
-    
-    let mut output = MadSkin::default();//no_style();
-    
+    console_api.print_output(out.unwrap());
+
     if process.status.success() {
-        //println!("ran {} PID {}", bin, status.code().unwrap());
-        output.bold.set_fg(crossterm::style::Color::Yellow);
-        output.italic.set_fg(crossterm::style::Color::Blue);
-        output.paragraph.set_fg(crossterm::style::Color::Green);
-
-        let text_template = TextTemplate::from(r#"
-        * ran: **${program}**
-        * code: **${status-code}**
-        * *args*: **${program-args} **
-        "#);
-
-        let mut expander = text_template.expander();
         let status_code = process.status.code().unwrap().to_string();
         let mut bin_args = String::new();
-        
-        for i in args{
-            let a:&str = i.as_ref();
+
+        for i in args {
+            let a: &str = i.as_ref();
             bin_args.push_str(a);
             bin_args.push_str("\n\t");
         }
-
-        expander
-            .set("program", bin)
-            .set("status-code", &status_code)
-            .set("program-args", &bin_args);
-        output.print_expander(expander);
+        console_api.print_exit_status_sucess(bin.to_owned(), status_code, bin_args)
+    } else {
+        console_api.print_exit_status_fail(bin.to_owned())
     }
-    else{
-        //println!("Failed to execute {}", bin);
-        
-        output.bold.set_fg(crossterm::style::Color::Red);
-        output.italic.set_fg(crossterm::style::Color::Grey);
 
-        let text_template = TextTemplate::from(r#"**Failed to execute**: *${program}*"#);
-        let mut expander = text_template.expander();
-
-        expander
-            .set("program", bin);
-        output.print_expander(expander);
-    }
-    println!(
-        "Finishing at {:02}:{:02}:{:02}",
-        hour,
-        now.minute(),
-        now.second(),
-    );
+    console_api.print_end();
 
     return process.status;
 }
